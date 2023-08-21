bb4a73fa408fc3eeea4f3e59752c21a08b6d598b
yousha-waja <youshawaja01@gmail.com>
2023-08-21 13:48:15 +0200

    added logging...

--- migrations/002.populate.taxi_queue.sql
+++ migrations/002.populate.taxi_queue.sql
@@ -1,5 +1,5 @@
-insert into taxi_queue (passenger_queue_count, taxi_queue_count)
-    values (0, 0); 
+/* insert into taxi_queue (passenger_queue_count, taxi_queue_count)
+    values (0, 0);  */
 
 /* select passenger_queue_count from taxi_queue; */
 
@@ -8,7 +8,7 @@ SET passenger_queue_count = 9;
 
  */
 
-/*  update taxi_queue set passenger_queue_count = passenger_queue_count +1 ; */
+/*  update taxi_queue set taxi_queue_count = 0; */
 
 /* update taxi_queue set taxi_queue_count = taxi_queue_count - 1, passenger_queue_count = passenger_queue_count-12
 where passenger_queue_count >12 AND taxi_queue_count > 5;
