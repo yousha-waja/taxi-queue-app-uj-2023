a95662ece1df6a98296d9d5c4971fa6f0bae1ae5
yousha-waja <youshawaja01@gmail.com>
2023-08-21 15:16:51 +0200

    taxi app without api

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -6,11 +6,32 @@ document.addEventListener('alpine:init', () => {
 			version: 'no-api-1.0',
 			queueCount:0,
 			taxiCount:0,
+			error:'',
+			showError:false,
 			leave(){
 				if(this.queueCount)
 				this.queueCount--;
-
-			}
+			},
+			depart() {
+                if (this.taxiCount >= 1 && this.queueCount >= 12) {
+                    this.queueCount -= 12;
+                    this.taxiCount--;
+                } else if (this.taxiCount <= 0) {
+                    this.showError=true;
+					this.error="There are no taxi's available!";
+					setTimeout(()=>{
+						this.showError=false;
+					    this.error="";
+					},5000);
+                } else if (this.queueCount < 12) {
+                    this.showError=true;
+					this.error="There are not enough passengers in the queue!";
+					setTimeout(()=>{
+						this.showError=false;
+					    this.error="";
+					},5000);
+                }
+            }
 		}
 
 	});
